fdf1eeb143dc0eb4d1996d7882dd0b515f0a7ec9
Kavia AI <production@kavia.ai>
2026-03-04 19:14:25 +0000

    Checkpoint 1 - cg022fe19c

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/smartrecipe-finder-237398-237413/snapchef_frontend
+flutter analyze
+
---
+++ snapchef_frontend/android/app/src/main/kotlin/com/example/snapchef_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.snapchef_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
